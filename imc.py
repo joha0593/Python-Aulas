5cccb34d261b94c245507f434d218187dcf315b0
Johanna Liza <141288432+joha0593@users.noreply.github.com>
2024-02-08 21:05:36 -0300

    Minha primeira aula de python
    Alguns codigos que estamos aprendendo na aula de python somo operadores logicos, tipos de variaveis, concatenacao

--- imc.py
+++ imc.py
@@ -3,3 +3,27 @@ peso = float(input("Digite seu peso: "))
 
 imc = peso / (altura ** 2 )
 print("IMC: ", round(imc,2))
+
+
+# Entrada dos dados
+peso = float(input("Digite o seu peso (kg):"))
+altura = float(input("Digite a sua altura (m):"))
+
+# Processamento
+imc = peso/(altura ** 2)
+
+# Classificacao do resultado
+if imc < 18.5:
+  resultado = "baixo peso"
+
+elif imc >= 18.5 and imc < 25:
+    resultado = "peso adequado"
+
+elif imc >= 18.5 and imc < 30:
+    resultado = "sobrepeso"
+
+else:
+  resultado = "obesidade"
+
+# Saida do dado
+print(f"O seu IMC esta em {round(imc,2)}, classificando-o como {resultado}.")
